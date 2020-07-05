4c1591aa0482520688575be04d653122e9da86ed
eXtremal-ik7 <eXtremal.ik7@gmail.com>
2020-07-05 14:30:53 +0300

    Start poolcore-0.3; move to hunter package manager & asyncio 0.5

--- src/cmake/FindFlatBuffers.cmake
+++
@@ -1,2 +0,0 @@
-find_path(FLATBUFFERS_INCLUDE_DIR "flatbuffers/flatbuffers.h")
-find_program(FLATC_COMMAND flatc)
--- src/cmake/Findleveldb.cmake
+++
@@ -1,7 +0,0 @@
-find_library(LEVELDB_LIBRARY leveldb
-  PATH ${ROOT_SOURCE_DIR}/leveldb-1.18
-)
-
-find_path(LEVELDB_INCLUDE_DIR "leveldb/db.h"
-  PATH ${ROOT_SOURCE_DIR}/leveldb-1.18/include
-)
