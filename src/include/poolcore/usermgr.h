cf30646a24a6d139af983a6f741f1fb2b2d004d1
eXtremal-ik7 <eXtremal.ik7@gmail.com>
2020-07-08 22:47:36 +0300

    UserManager: introduce asynchronous calls

--- src/include/poolcore/usermgr.h
+++ src/include/poolcore/usermgr.h
@@ -2,9 +2,20 @@
 
 #include "kvdb.h"
 #include "poolcore/rocksdbBase.h"
+#include "asyncio/asyncio.h"
+#include <tbb/concurrent_queue.h>
+#include <thread>
 
 class UserManager {
 public:
+  struct Credentials {
+    std::string Login;
+    std::string Password;
+    std::string Name;
+    std::string EMail;
+    std::string TwoFactor;
+  };
+
   struct UserInfo {
     std::string Name;
     std::string EMail;
@@ -16,13 +27,47 @@ public:
     bool AutoPayoutEnabled;
   };
 
+  // Asynchronous api
+  class Task {
+  public:
+    virtual void run(UserManager *userMgr) = 0;
+    virtual ~Task() {}
+  };
+
+  class UserCreateTask : public Task {
+  public:
+    UserCreateTask(Credentials &&credentials, std::function<void(bool, const std::string&)> callback) : Credentials_(credentials), Callback_(callback) {}
+    void run(UserManager *userMgr) final {
+      userMgr->userCreateImpl(Credentials_, Callback_);
+    }
+  private:
+    Credentials Credentials_;
+    std::function<void(bool, const std::string&)> Callback_;
+  };
+
 public:
   UserManager(const std::filesystem::path &dbPath);
+  void start();
+  void stop();
 
+  // Asynchronous api
+  void userCreate(Credentials &&credentials, std::function<void(bool, const std::string&)> callback) { startAsyncTask(new UserCreateTask(std::move(credentials), callback)); }
+
+  // Synchronous api
   bool getUserInfo(const std::string &login, UserInfo &info);
   bool getUserCoinSettings(const std::string &login, const std::string &coin, UserCoinSettings &settings);
 
 private:
+  // Asynchronous api implementation
+  void startAsyncTask(Task *task);
+  void userCreateImpl(const Credentials &credentials, std::function<void(bool, const std::string&)> callback);
+
+  void userManagerMain();
+
+  asyncBase *Base_;
+  aioUserEvent *TaskQueueEvent_;
+  tbb::concurrent_queue<Task*> Tasks_;
   kvdb<rocksdbBase> UsersDb_;
   kvdb<rocksdbBase> UserSettingsDb_;
+  std::thread Thread_;
 };
--- src/poolcore/usermgr.cpp
+++ src/poolcore/usermgr.cpp
@@ -1,10 +1,46 @@
 #include "poolcore/usermgr.h"
+#include "loguru.hpp"
 
 UserManager::UserManager(const std::filesystem::path &dbPath) :
   UsersDb_(dbPath / "users"),
   UserSettingsDb_(dbPath / "usersettings")
 {
+  Base_ = createAsyncBase(amOSDefault);
+  TaskQueueEvent_ = newUserEvent(Base_, 0, [](aioUserEvent*, void *arg) {
+    Task *task = nullptr;
+    UserManager *userMgr = static_cast<UserManager*>(arg);
+    while (userMgr->Tasks_.try_pop(task))
+      task->run(userMgr);
+  }, this);
+}
 
+void UserManager::start()
+{
+  Thread_ = std::thread([](UserManager *userMgr){ userMgr->userManagerMain(); }, this);
+}
+
+void UserManager::stop()
+{
+  postQuitOperation(Base_);
+  Thread_.join();
+}
+
+void UserManager::userManagerMain()
+{
+  loguru::set_thread_name("UserManager");
+  asyncLoop(Base_);
+}
+
+void UserManager::startAsyncTask(Task *task)
+{
+  Tasks_.push(task);
+  userEventActivate(TaskQueueEvent_);
+}
+
+void UserManager::userCreateImpl(const Credentials &credentials, std::function<void(bool, const std::string&)> callback)
+{
+  LOG_F(ERROR, "userCreateImpl: not implemented");
+  callback(false, "userCreateImpl: not implemented");
 }
 
 bool UserManager::getUserInfo(const std::string &login, UserInfo &info)
