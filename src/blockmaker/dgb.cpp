2b271f03c3dfa997d855f82f4258a4477ab613d9
eXtremal-ik7 <eXtremal.ik7@gmail.com>
2023-07-19 22:56:11 +0700

    DGB: qubit, skein & odo support

--- src/blockmaker/dgb.cpp
+++ src/blockmaker/dgb.cpp
@@ -1,12 +1,143 @@
+#include "poolcommon/arith_uint256.h"
 #include "blockmaker/dgb.h"
+#include "blockmaker/sph_skein.h"
+#include "blockmaker/sph_luffa.h"
+#include "blockmaker/sph_cubehash.h"
+#include "blockmaker/sph_shavite.h"
+#include "blockmaker/sph_simd.h"
+#include "blockmaker/sph_echo.h"
+#include "blockmaker/odocrypt.h"
+#include "blockmaker/KeccakP-800-SnP.h"
+
+static uint32_t OdoKey(uint32_t nOdoShapechangeInterval, uint32_t nTime)
+{
+  uint32_t nShapechangeInterval = nOdoShapechangeInterval;
+  return nTime - nTime % nShapechangeInterval;
+}
 
 namespace DGB {
 
-bool Proto::checkConsensus(const DGB::Proto::BlockHeader&, CheckConsensusCtx&, DGB::Proto::ChainParams&, double *shareDiff)
+template<> bool Proto<DGB::Algo::EQubit>::checkConsensus(const Proto<DGB::Algo::EQubit>::BlockHeader &header, CheckConsensusCtx&, DGB::Proto<DGB::Algo::EQubit>::ChainParams&, double *shareDiff)
 {
-  // TODO: implement
-  *shareDiff = 0;
-  return false;
+  arith_uint256 result;
+  sph_luffa512_context	 ctx_luffa;
+  sph_cubehash512_context  ctx_cubehash;
+  sph_shavite512_context	 ctx_shavite;
+  sph_simd512_context		 ctx_simd;
+  sph_echo512_context		 ctx_echo;
+  uint512 hash[5];
+
+  sph_luffa512_init(&ctx_luffa);
+  sph_luffa512(&ctx_luffa, &header, sizeof(Proto<DGB::Algo::ESkein>::BlockHeader));
+  sph_luffa512_close(&ctx_luffa, &hash[0]);
+
+  sph_cubehash512_init(&ctx_cubehash);
+  sph_cubehash512 (&ctx_cubehash, static_cast<const void*>(&hash[0]), 64);
+  sph_cubehash512_close(&ctx_cubehash, static_cast<void*>(&hash[1]));
+
+  sph_shavite512_init(&ctx_shavite);
+  sph_shavite512(&ctx_shavite, static_cast<const void*>(&hash[1]), 64);
+  sph_shavite512_close(&ctx_shavite, static_cast<void*>(&hash[2]));
+
+  sph_simd512_init(&ctx_simd);
+  sph_simd512 (&ctx_simd, static_cast<const void*>(&hash[2]), 64);
+  sph_simd512_close(&ctx_simd, static_cast<void*>(&hash[3]));
+
+  sph_echo512_init(&ctx_echo);
+  sph_echo512 (&ctx_echo, static_cast<const void*>(&hash[3]), 64);
+  sph_echo512_close(&ctx_echo, static_cast<void*>(&hash[4]));
+
+  memcpy(result.begin(), hash[4].begin(), 32);
+
+  *shareDiff = BTC::difficultyFromBits(result.GetCompact(), 29);
+
+  bool fNegative;
+  bool fOverflow;
+  arith_uint256 bnTarget;
+
+  bnTarget.SetCompact(header.nBits, &fNegative, &fOverflow);
+
+  // Check range
+  if (fNegative || bnTarget == 0 || fOverflow)
+    return false;
+
+  // Check proof of work matches claimed amount
+  if (result > bnTarget)
+    return false;
+
+  return true;
+}
+
+template<> bool Proto<DGB::Algo::ESkein>::checkConsensus(const Proto<DGB::Algo::ESkein>::BlockHeader &header, CheckConsensusCtx&, DGB::Proto<DGB::Algo::ESkein>::ChainParams&, double *shareDiff)
+{
+  SHA256_CTX sha256Context;
+  sph_skein512_context skeinContext;
+  uint512 skeinHash;
+  arith_uint256 result;
+
+  sph_skein512_init(&skeinContext);
+  sph_skein512(&skeinContext, &header, sizeof(Proto<DGB::Algo::ESkein>::BlockHeader));
+  sph_skein512_close(&skeinContext, &skeinHash);
+
+  SHA256_Init(&sha256Context);
+  SHA256_Update(&sha256Context, skeinHash.begin(), skeinHash.size());
+  SHA256_Final(result.begin(), &sha256Context);
+
+  SHA256_Init(&sha256Context);
+  SHA256_Update(&sha256Context, result.begin(), sizeof(result));
+  SHA256_Final(result.begin(), &sha256Context);
+
+  *shareDiff = BTC::difficultyFromBits(result.GetCompact(), 29);
+
+  bool fNegative;
+  bool fOverflow;
+  arith_uint256 bnTarget;
+
+  bnTarget.SetCompact(header.nBits, &fNegative, &fOverflow);
+
+  // Check range
+  if (fNegative || bnTarget == 0 || fOverflow)
+    return false;
+
+  // Check proof of work matches claimed amount
+  if (result > bnTarget)
+    return false;
+
+  return true;
+}
+
+template<> bool Proto<DGB::Algo::EOdo>::checkConsensus(const Proto<DGB::Algo::EOdo>::BlockHeader &header, CheckConsensusCtx &ctx, DGB::Proto<DGB::Algo::EOdo>::ChainParams&, double *shareDiff)
+{
+  uint32_t key = OdoKey(ctx.OdoShapechangeInterval, header.nTime);
+
+  char cipher[KeccakP800_stateSizeInBytes] = {};
+  arith_uint256 result;
+
+  size_t len = sizeof(Proto<DGB::Algo::EOdo>::BlockHeader);
+  memcpy(cipher, &header, len);
+  cipher[len] = 1;
+
+  OdoCrypt(key).Encrypt(cipher, cipher);
+  KeccakP800_Permute_12rounds(cipher);
+  memcpy(result.begin(), cipher, result.size());
+
+  *shareDiff = BTC::difficultyFromBits(result.GetCompact(), 29);
+
+  bool fNegative;
+  bool fOverflow;
+  arith_uint256 bnTarget;
+
+  bnTarget.SetCompact(header.nBits, &fNegative, &fOverflow);
+
+  // Check range
+  if (fNegative || bnTarget == 0 || fOverflow)
+    return false;
+
+  // Check proof of work matches claimed amount
+  if (result > bnTarget)
+    return false;
+
+  return true;
 }
 
 }
--- src/include/blockmaker/dgb.h
+++ src/include/blockmaker/dgb.h
@@ -3,6 +3,13 @@
 #include "btc.h"
 
 namespace DGB {
+enum class Algo {
+  EQubit,
+  ESkein,
+  EOdo
+};
+
+template<Algo algo>
 class Proto {
 public:
   static constexpr const char *TickerName = "DGB";
@@ -17,16 +24,30 @@ public:
   using TxWitness = BTC::Proto::TxWitness;
   using Transaction = BTC::Proto::Transaction;
 
-  using CheckConsensusCtx = BTC::Proto::CheckConsensusCtx;
+  struct CheckConsensusCtx {
+    void initialize(const std::string &ticker) {
+      if (ticker.find(".testnet")) {
+        OdoShapechangeInterval = 1*24*60*60; // 1 day
+      } else if (ticker.find(".regtest") != ticker.npos) {
+        OdoShapechangeInterval = 4; // 1 minute
+      } else {
+        // mainnet
+        OdoShapechangeInterval = 10*24*60*60; // 10 days
+      }
+    }
+    uint32_t OdoShapechangeInterval;
+  };
+
   using ChainParams = BTC::Proto::ChainParams;
 
   static void checkConsensusInitialize(CheckConsensusCtx&) {}
-  static bool checkConsensus(const DGB::Proto::BlockHeader&, CheckConsensusCtx&, DGB::Proto::ChainParams&, double *shareDiff);
-  static bool checkConsensus(const DGB::Proto::Block &block, CheckConsensusCtx &ctx, DGB::Proto::ChainParams &chainParams, double *shareDiff) { return checkConsensus(block.header, ctx, chainParams, shareDiff); }
+  static bool checkConsensus(const BlockHeader&, CheckConsensusCtx&, ChainParams&, double *shareDiff);
+  static bool checkConsensus(const Block &block, CheckConsensusCtx &ctx, ChainParams &chainParams, double *shareDiff) { return checkConsensus(block.header, ctx, chainParams, shareDiff); }
   static double getDifficulty(const Proto::BlockHeader &header) { return BTC::difficultyFromBits(header.nBits, 29); }
   static bool decodeHumanReadableAddress(const std::string &hrAddress, const std::vector<uint8_t> &pubkeyAddressPrefix, AddressTy &address) { return BTC::Proto::decodeHumanReadableAddress(hrAddress, pubkeyAddressPrefix, address); }
 };
 
+template<Algo algo>
 class Stratum {
 public:
   static constexpr double DifficultyFactor = 65536.0;
@@ -34,9 +55,9 @@ public:
   using MiningConfig = BTC::Stratum::MiningConfig;
   using WorkerConfig = BTC::Stratum::WorkerConfig;
   using StratumMessage = BTC::Stratum::StratumMessage;
-  using Work = BTC::WorkTy<DGB::Proto, BTC::Stratum::HeaderBuilder, BTC::Stratum::CoinbaseBuilder, BTC::Stratum::Notify, BTC::Stratum::Prepare, MiningConfig, WorkerConfig, StratumMessage>;
-  using SecondWork = StratumSingleWorkEmpty<Proto::BlockHashTy, MiningConfig, WorkerConfig, StratumMessage>;
-  using MergedWork = StratumMergedWorkEmpty<Proto::BlockHashTy, MiningConfig, WorkerConfig, StratumMessage>;
+  using Work = BTC::WorkTy<Proto<algo>, BTC::Stratum::HeaderBuilder, BTC::Stratum::CoinbaseBuilder, BTC::Stratum::Notify, BTC::Stratum::Prepare, MiningConfig, WorkerConfig, StratumMessage>;
+  using SecondWork = StratumSingleWorkEmpty<typename Proto<algo>::BlockHashTy, MiningConfig, WorkerConfig, StratumMessage>;
+  using MergedWork = StratumMergedWorkEmpty<typename Proto<algo>::BlockHashTy, MiningConfig, WorkerConfig, StratumMessage>;
 
   static constexpr bool MergedMiningSupport = false;
   static bool isMainBackend(const std::string&) { return true; }
@@ -44,9 +65,10 @@ public:
   static void buildSendTargetMessage(xmstream &stream, double difficulty) { BTC::Stratum::buildSendTargetMessageImpl(stream, difficulty, DifficultyFactor); }
 };
 
+template<Algo algo>
 struct X {
-  using Proto = DGB::Proto;
-  using Stratum = DGB::Stratum;
+  using Proto = DGB::Proto<algo>;
+  using Stratum = DGB::Stratum<algo>;
   template<typename T> static inline void serialize(xmstream &src, const T &data) { BTC::Io<T>::serialize(src, data); }
   template<typename T> static inline void unserialize(xmstream &dst, T &data) { BTC::Io<T>::unserialize(dst, data); }
 };
