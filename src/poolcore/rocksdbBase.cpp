0e8d07eda51a7f56e311f4ccf34d81a5b74ac2fb
eXtremal-ik7 <eXtremal.ik7@gmail.com>
2026-02-27 13:23:10 +0300

    reduce rocksdb old log files

--- src/poolcore/rocksdbBase.cpp
+++ src/poolcore/rocksdbBase.cpp
@@ -159,6 +159,7 @@ rocksdb::DB *rocksdbBase::open(rocksdbBase::partition &partition)
       rocksdb::Options options;
       options.create_if_missing = true;
       options.compression = rocksdb::kZSTD;
+      options.keep_log_file_num = 4;
       if (MergeOperator_)
         options.merge_operator = MergeOperator_;
       rocksdb::Status status = rocksdb::DB::Open(options, path_to_utf8(partitionPath), &partition.db);
